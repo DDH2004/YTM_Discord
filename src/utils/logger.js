58424ae31f85508f2634ac6c349acd6900758cb6
Akhil Devarasetty <35643940+DDH2004@users.noreply.github.com>
2025-04-10 08:03:54 +0000

    Refactor logger utility functions for consistency and clarity

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,23 +1,33 @@
 // This file provides utility functions for logging messages, which can be used throughout the extension for debugging purposes.
 
-const DEBUG = true;
+const debugMode = true;
 
-function log(...args) {
-  if (DEBUG) {
-    console.log("[YTMusic Discord]", ...args);
+function debugLog(...args) {
+  if (debugMode) {
+    console.log('[YTM Discord]', ...args);
   }
 }
 
+function log(...args) {
+  console.log('[YTM Discord]', ...args);
+}
+
 function error(...args) {
-  console.error("[YTMusic Discord Error]", ...args);
+  console.error('[YTM Discord]', ...args);
 }
 
 function info(...args) {
-  console.info("[YTMusic Discord Info]", ...args);
+  console.info('[YTM Discord]', ...args);
 }
 
 function warn(...args) {
-  console.warn("[YTMusic Discord Warning]", ...args);
+  console.warn('[YTM Discord]', ...args);
 }
 
-export { log, error, info, warn };
+module.exports = {
+  debugLog,
+  log,
+  error,
+  info,
+  warn
+};
